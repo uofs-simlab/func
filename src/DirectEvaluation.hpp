9df64e364d00b7af4ce5eed9860981accc55d589
Shawn Samuel Carl McAdam <Shawn@SadMachine.local>
2020-05-11 18:00:32 -0600

    The DirectEvaluation class is now equipped to record all its given args and plot them on a histogram if the -DFUNC_RECORD flag is specified at compile time.

--- src/DirectEvaluation.hpp
+++ src/DirectEvaluation.hpp
@@ -10,14 +10,33 @@
   - requires a max and min value (for consistency with EvaluationImplementation)
 */
 #pragma once
+
+/* A macro for recording where the given function is being evaluated.  */
+#ifdef FUNC_RECORD
+  #define FUNC_RECORD_ARG(X) (record_arg(X))
+#else
+  #define FUNC_RECORD_ARG(X)
+#endif
+
 #include "EvaluationImplementation.hpp"
 #include <cmath>
 
 class DirectEvaluation final : public EvaluationImplementation
 {
+
+private:
+  // the fields methods needed to record where the function is being evaluated
+  unsigned int *m_histogram;
+  unsigned int m_histSize; 
+  double m_peak;
+  unsigned int calcIndex(double x);
+  void record_arg(double x);
+  std::string histogramToString();
+ 
 public:
-  DirectEvaluation(EvaluationFunctor<double,double> *func, double min = 0, double max = 1);
+  DirectEvaluation(EvaluationFunctor<double,double> *func, double min = 0, double max = 1, unsigned int histSize = 10);
   double operator()(double x) override;
-  ~DirectEvaluation();
   void print_details(std::ostream& out) override;
+  void print_details_json(std::ostream& out);
+  ~DirectEvaluation();
 };
--- src/StdRng.hpp
+++ src/StdRng.hpp
@@ -1,30 +1,35 @@
+#pragma once
+
 #include <iostream>
 #include <random>
 #include "RngInterface.hpp"
 
-#pragma once
-
-// An implementation of RngInterface for sampling from the distributions defined in std::random.
+/* An implementation of RngInterface for sampling from 
+   the distributions defined in std::random. */
 template <class DistType>
 class StdRng : public RngInterface<> 
 {
-  std::unique_ptr<DistType> mp_distribution=nullptr;
+  std::unique_ptr<DistType> mp_distribution;
   unsigned int m_seed;
-  std::unique_ptr<std::mt19937> mp_generator=nullptr;
+  std::unique_ptr<std::mt19937> mp_generator;
 
   public:
-    StdRng(DistType *dist) : mp_distribution(std::move(dist)) {}
+    // create a new StdRng based on the pointer to random distribution passed in
+    StdRng(DistType *dist) : mp_distribution(std::move(dist)){}
 
+    // set the seed and generator of the distribution
     void init(unsigned int seed)
     {
       m_seed = seed;
       mp_generator.reset(new std::mt19937(seed));
     }
 
+    // return the seed
     unsigned int seed(){ return m_seed; }
 
+    // get a random point from the given distribution
     double getPt(){ return (*mp_distribution)(*mp_generator); }
-    
+
     ~StdRng(){}
 };
 
