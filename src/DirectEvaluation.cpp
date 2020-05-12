9b3858b78d5b36fe106f5ccd61054bc2abe1c570
Shawn Samuel Carl McAdam <Shawn@SadMachine.local>
2020-05-12 16:20:49 -0600

    Moved everything dealing with plotting function arguments out of DirectEvaluation and into a new class called ArgumentRecord.

--- src/DirectEvaluation.cpp
+++ src/DirectEvaluation.cpp
@@ -2,6 +2,10 @@
 #include "DirectEvaluation.hpp"
 #include "json.hpp"
 
+#ifdef FUNC_RECORD
+#include "ArgumentRecord.hpp"
+#endif
+
 DirectEvaluation::DirectEvaluation(EvaluationFunctor<double,double> *func, double min, double max, unsigned int histSize) : 
   EvaluationImplementation(func, "DirectEvaluation")
 {
@@ -10,29 +14,9 @@ DirectEvaluation::DirectEvaluation(EvaluationFunctor<double,double> *func, doubl
   this->m_dataSize = 0;
 
   #ifdef FUNC_RECORD
-  /* variables needed for recording function arguments */
-  this->m_histogram=new unsigned int[histSize];
-  std::fill(m_histogram, m_histogram+histSize, 0);
-  this->m_histSize=histSize;
-  this->m_peak=0;
+    mp_recorder=std::unique_ptr<ArgumentRecord>(new ArgumentRecord(histSize, min, max));
   #endif
   (void) histSize; // don't let the compiler complain if -DFUNC_RECORD isn't specified
-}
-
-void DirectEvaluation::record_arg(double x)
-{
-#ifdef FUNC_RECORD
-  unsigned int index = calcIndex(x);
-  m_histogram[index]++;
-  unsigned int peak_index = calcIndex(m_peak);
-  if(m_histogram[index]>m_histogram[peak_index])
-    m_peak=x;
-#endif
-}
-
-unsigned int DirectEvaluation::calcIndex(double x)
-{
-  return ((unsigned int) (m_histSize*(x-m_minArg)/(m_maxArg+1-m_minArg)))%m_histSize;
 }
 
 void DirectEvaluation::print_details_json(std::ostream& out)
@@ -43,64 +27,26 @@ void DirectEvaluation::print_details_json(std::ostream& out)
   jsonStats["name"] = m_name;
   jsonStats["minArg"] = m_minArg;
   jsonStats["maxArg"] = m_maxArg;
-#ifdef FUNC_RECORD
-  jsonStats["_comment"] = "Histogram of function evaluations.";
-  jsonStats["histogramSize"] = m_histSize;
-  jsonStats["histogram"]=histogramToString();
-  jsonStats["peak"] = m_histogram[calcIndex(m_peak)];
-  jsonStats["peakLoc"] = calcIndex(m_peak);
-  //other statistics
-#endif
+  #ifdef FUNC_RECORD
+    mp_recorder->print_details_json(out);
+  #endif
   out << jsonStats.dump(2) << std::endl;
 }
 
 double DirectEvaluation::operator()(double x)
 {
-  // FUNC_RECORD_ARG is a null operator is -DFUNC_RECORD is not 
-  // specified at compile time 
-  FUNC_RECORD_ARG(x);
+  #ifdef FUNC_RECORD
+    mp_recorder->record_arg(x);
+  #endif
   return (*mp_func)(x);
-}
-
-std::string DirectEvaluation::histogramToString()
-{
-#ifdef FUNC_RECORD
-  std::string hist_str;
-  hist_str=hist_str+std::to_string(m_minArg)+'\n';
-  for(unsigned int i=0; i<m_histSize; i++){
-    for(unsigned int j=0; j<(unsigned int)(15*m_histogram[i]/m_histogram[calcIndex(m_peak)]); j++)
-      hist_str=hist_str+'*';
-    hist_str=hist_str+'\n';
-  }
-  hist_str=hist_str+std::to_string(m_maxArg);
-  return hist_str;
-#else
-  return "";
-#endif
 }
 
 void DirectEvaluation::print_details(std::ostream& out)
 {
-#ifdef FUNC_RECORD
-  // output a histogram if the function has been evaluated at least once
-  unsigned int peak_idx=calcIndex(m_peak);
-  if(m_histogram[peak_idx]==0){
-    out << m_name << " " << m_minArg << " " << m_maxArg << " " << std::endl;
-    return;
-  }
-  out << m_name << std::endl;
-  out<<"histogram of evaluations:"<<std::endl;
-  out<<histogramToString()<<std::endl;
-  out<<"The peak is located at x=" << m_peak << " with ";
-  out<< m_histogram[peak_idx] << " evaluations." << std::endl;
-#else 
   out<< m_name << " " << m_minArg << " " << m_maxArg << " " << std::endl;
-#endif
+  #ifdef FUNC_RECORD
+    mp_recorder->print_details(out);
+  #endif
 }
 
-DirectEvaluation::~DirectEvaluation()
-{
-#ifdef FUNC_RECORD
-  delete m_histogram;
-#endif
-}
+DirectEvaluation::~DirectEvaluation(){}
--- src/DirectEvaluation.hpp
+++ src/DirectEvaluation.hpp
@@ -8,31 +8,18 @@
   Notes:
   - basically just a wrapper around a function
   - requires a max and min value (for consistency with EvaluationImplementation)
+  - Record where the function is being evaluated by specifying the -DFUNC_RECORD flag at compile time
 */
 #pragma once
 
-/* A macro for recording where the given function is being evaluated.  */
-#ifdef FUNC_RECORD
-  #define FUNC_RECORD_ARG(X) (record_arg(X))
-#else
-  #define FUNC_RECORD_ARG(X)
-#endif
-
 #include "EvaluationImplementation.hpp"
+#include "ArgumentRecord.hpp"
 #include <cmath>
 
 class DirectEvaluation final : public EvaluationImplementation
-{
-
+{  
 private:
-  // the fields methods needed to record where the function is being evaluated
-  unsigned int *m_histogram;
-  unsigned int m_histSize; 
-  double m_peak;
-  unsigned int calcIndex(double x);
-  void record_arg(double x);
-  std::string histogramToString();
- 
+  std::unique_ptr<ArgumentRecord> mp_recorder;
 public:
   DirectEvaluation(EvaluationFunctor<double,double> *func, double min = 0, double max = 1, unsigned int histSize = 10);
   double operator()(double x) override;
