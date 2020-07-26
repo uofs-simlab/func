477409fddaeff5fb71b06d2b203065fa9d46ba10
Shawn Samuel Carl McAdam <Shawn@SadMachine.local>
2020-07-26 17:50:36 -0600

    Fixed the interpolation

--- src/table_types/NonUniformLinearInterpolationTable.cpp
+++ src/table_types/NonUniformLinearInterpolationTable.cpp
@@ -12,11 +12,11 @@ NonUniformLinearInterpolationTable::NonUniformLinearInterpolationTable(FunctionC
   m_order = 2;
   m_numTableEntries = m_numIntervals;
   m_dataSize = (unsigned) sizeof(m_table[0]) * m_numTableEntries;
-
+  
   /* Allocate and set table */
   m_table.reset(new polynomial<1,8>[m_numTableEntries]);
   for (int ii=0; ii<m_numTableEntries; ++ii) {
-    const double x = m_minArg + m_g(ii/(m_numTableEntries-1))*(m_maxArg-m_minArg);
+    const double x = m_minArg + m_g(ii/(double)(m_numTableEntries-1))*(m_maxArg-m_minArg);
     m_grid[ii]  = x;
     m_table[ii].coefs[0] = mp_func(x);
   }
@@ -24,13 +24,14 @@ NonUniformLinearInterpolationTable::NonUniformLinearInterpolationTable(FunctionC
 
 double NonUniformLinearInterpolationTable::operator()(double x)
 {
-  double   dx = (m_numTableEntries-1)*m_g_inv((x-m_minArg)/(m_maxArg-m_minArg));
-  // index of previous table entry
-  unsigned x1  = (unsigned) dx;
+  // TODO simplify I hope
+  double x_idx = (unsigned) (m_numTableEntries-1)*m_g_inv((x-m_minArg)/(m_maxArg-m_minArg));
+  double h     = m_grid[x_idx+1] - m_grid[x_idx];
+  double dx    = (x - m_grid[x_idx])/h;
+
   // value of table entries around x position
-  dx -= x1;
-  double   y1  = m_table[x1].coefs[0];
-  double   y2  = m_table[x1+1].coefs[0];
+  double   y1  = m_table[x_idx].coefs[0];
+  double   y2  = m_table[x_idx+1].coefs[0];
   // linear interpolation
   return y1+dx*(y2-y1);
 }
