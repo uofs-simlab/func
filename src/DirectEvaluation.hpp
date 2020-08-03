1982202eee6fc486a45eee766ebf81e6f5336978
Shawn Samuel Carl McAdam <Shawn@SadMachine.local>
2020-08-03 14:32:35 -0600

    moved some method declarations outside of the class

--- src/DirectEvaluation.hpp
+++ src/DirectEvaluation.hpp
@@ -9,7 +9,9 @@
   Notes:
   - basically just a wrapper around a function
   - requires a max and min value (for consistency with EvaluationImplementation)
-  - Record where the function is being evaluated by specifying the -DFUNC_RECORD flag at compile time
+  - Record where the function is being evaluated by specifying the -DFUNC_RECORD
+  flag at compile time. ArgumentRecorder is an extension of this class which does
+  all the work recording function arguments.
 */
 #pragma once
 
@@ -31,8 +33,8 @@ private:
   #endif
 public:
 
-  // set base class args and setup argument recording if 
-  // -DFUNC_RECORD is used at compile time
+  /* set base class args and set up argument recording if 
+     -DFUNC_RECORD is used at compile time */
   DirectEvaluation(FunctionContainer<IN_TYPE,OUT_TYPE> *func_container,
       IN_TYPE min = 0, IN_TYPE max = 1, unsigned int histSize = 10) :
     EvaluationImplementation<IN_TYPE,OUT_TYPE>(func_container->standard_func, "DirectEvaluation")
@@ -41,7 +43,7 @@ public:
     #ifdef FUNC_RECORD
       mp_recorder = std::unique_ptr<ArgumentRecord>(new ArgumentRecord(histSize, min, max));
     #endif
-    (void) histSize; // don't let the compiler complain if -DFUNC_RECORD isn't specified
+    (void) histSize; // ignore histSize if -DFUNC_RECORD isn't specified
   }
 
   // Evaluate the underlying std::function and optionally record the arg
@@ -53,28 +55,32 @@ public:
     return this->mp_func(x);
   }
 
-  void print_details_json(std::ostream& out)
-  {
-    using nlohmann::json;
-    json jsonStats;
-
-    jsonStats["name"] = this->m_name;
-    jsonStats["minArg"] = this->m_minArg;
-    jsonStats["maxArg"] = this->m_maxArg;
-    #ifdef FUNC_RECORD
-      mp_recorder->print_details_json(out);
-    #endif
-    out << jsonStats.dump(2) << std::endl;
-  }
-
-  void print_details(std::ostream& out) override
-  {
-    out<< this->m_name << " " << this->m_minArg << " " << this->m_maxArg;
-    #ifdef FUNC_RECORD
-      out << std::endl;
-      mp_recorder->print_details(out);
-    #endif
-  }
-
+  void print_details(std::ostream& out) override;
+  void print_details_json(std::ostream& out);
   ~DirectEvaluation(){};
 };
+
+template <typename IN_TYPE, typename OUT_TYPE>
+inline void DirectEvaluation<IN_TYPE,OUT_TYPE>::print_details(std::ostream& out)
+{
+  out<< this->m_name << " " << this->m_minArg << " " << this->m_maxArg;
+  #ifdef FUNC_RECORD
+    out << std::endl;
+    mp_recorder->print_details(out);
+  #endif
+}
+
+template <typename IN_TYPE, typename OUT_TYPE>
+inline void DirectEvaluation<IN_TYPE,OUT_TYPE>::print_details_json(std::ostream& out)
+{
+  using nlohmann::json;
+  json jsonStats;
+
+  jsonStats["name"] = this->m_name;
+  jsonStats["minArg"] = this->m_minArg;
+  jsonStats["maxArg"] = this->m_maxArg;
+  #ifdef FUNC_RECORD
+    mp_recorder->print_details_json(out);
+  #endif
+  out << jsonStats.dump(2) << std::endl;
+}
