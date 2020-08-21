56f2504c26c11cfd735826c82a08ff15054d5a6e
Shawn Samuel Carl McAdam <Shawn@SadMachine.local>
2020-08-21 15:24:38 -0600

    store the index of the histogram's peak rather than one of the arguments recorded in that subinterval

--- src/ArgumentRecord.hpp
+++ src/ArgumentRecord.hpp
@@ -18,6 +18,7 @@
 #include <memory>
 #include <mutex>
 #include <fstream>
+#include <limits> // numeric_limits<IN_TYPE>::max() / min()
 #include "json.hpp"
 
 // macro used to decide where an arg should be placed in the histogram
@@ -33,14 +34,16 @@ class ArgumentRecord
   std::unique_ptr<std::mutex[]> mp_histogram_mutex;
   unsigned int                  m_histSize;
 
-  // min and max should always be the same as the EvaluationImpl
-  // containing the ArgumentRecord
+  // set min/max to the min/max of the encapsulating EvaluationImplementation.
+  // Or use std::numeric_limits<IN_TYPE>::max()/ min()
+  // to make the histogram account for all possible values of x.
+  // At the price of making the histogram less readable
   IN_TYPE m_minArg;
   IN_TYPE m_maxArg;
 
   // vars containing any statistics each with a corresponding
   // mutex to keep things threadsafe
-  IN_TYPE m_peak_arg;
+  IN_TYPE m_peak_index; // the index of the bucket with the largest count
   std::mutex m_peak_mutex;
 
   // Record the extreme args so the user
@@ -61,16 +64,16 @@ class ArgumentRecord
       mp_histogram_mutex = std::unique_ptr<std::mutex[]>(new std::mutex[histSize]);
 
       // naive initial member arg values
-      m_peak_arg=m_minArg;
-      m_max_recorded=-std::numeric_limits<IN_TYPE>::max();
-      m_min_recorded=std::numeric_limits<IN_TYPE>::max();
+      m_peak_index = 0.0;
+      m_max_recorded = -std::numeric_limits<IN_TYPE>::max();
+      m_min_recorded =  std::numeric_limits<IN_TYPE>::max();
     }
 
     ~ArgumentRecord(){}
 
     // Rebuild our argument record
     // Assuming the encapsulating EvaluationImplementation
-    // gave us a valid json file
+    // gave us a valid json object
     ArgumentRecord(nlohmann::json jsonStats)
     {
       m_minArg = jsonStats["ArgumentRecord"]["minArg"].get<IN_TYPE>();
@@ -82,28 +85,27 @@ class ArgumentRecord
       // restore our histogram's thread safety
       mp_histogram_mutex = std::vector<std::unique_ptr<std::mutex>>(m_histSize, std::unique_ptr<std::mutex>(new std::mutex));
 
-      m_peak_arg = jsonStats["ArgumentRecord"]["peak_arg"].get<IN_TYPE>();
+      m_peak_index = jsonStats["ArgumentRecord"]["peak_index"].get<unsigned int>();
       m_max_recorded = jsonStats["ArgumentRecord"]["m_min_recorded"].get<IN_TYPE>();
       m_min_recorded = jsonStats["ArgumentRecord"]["m_max_recorded"].get<IN_TYPE>();
     }
 
-    // place x in the histogram
+    /* place x in the histogram */
     void record_arg(IN_TYPE x)
     {
       // Record x if it's within our table's limits
       // note: each std::lock_guard only exists for the duration of its scope
       if(m_minArg <= x && x <= m_maxArg){
-        int index = COMPUTE_INDEX(x);
+        int m_index = COMPUTE_INDEX(x);
         {
-          std::lock_guard<std::mutex> lock(mp_histogram_mutex[index]);
-          mv_histogram[index]++;
+          std::lock_guard<std::mutex> lock(mp_histogram_mutex[x_index]);
+          mv_histogram[x_index]++;
         }
 
         {
           std::lock_guard<std::mutex> lock(m_peak_mutex);
-          unsigned int peak_index = COMPUTE_INDEX(m_peak_arg);
-          if(mv_histogram[index] > mv_histogram[peak_index])
-            m_peak_arg = x;
+          if(mv_histogram[x_index] > mv_histogram[m_peak_index])
+            m_peak_index = x_index;
         } 
       }
 
@@ -135,15 +137,14 @@ template <typename IN_TYPE>
 inline std::string ArgumentRecord<IN_TYPE>::to_string()
 {
   // avoid division by zero by printing nothing if the histogram is empty
-  unsigned int peak_index=COMPUTE_INDEX(m_peak_arg);
-  if(mv_histogram[peak_index]==0)
+  if(mv_histogram[m_peak_index]==0)
     return "";
   
   // print out the histogram horizontally such that the longest row is 15 stars wide
   std::string hist_str;
   hist_str=std::to_string(m_minArg)+'\n';
   for(unsigned int i=0; i<m_histSize; i++){
-    for(unsigned int j=0; j<(unsigned int)(15*mv_histogram[i]/mv_histogram[peak_index]); j++)
+    for(unsigned int j=0; j<(unsigned int)(15*mv_histogram[i]/mv_histogram[m_peak_index]); j++)
       hist_str=hist_str+'*';
     hist_str=hist_str+'\n';
   }
@@ -154,12 +155,13 @@ inline std::string ArgumentRecord<IN_TYPE>::to_string()
 template <typename IN_TYPE>
 inline void ArgumentRecord<IN_TYPE>::print_details_json(nlohmann::json& jsonStats)
 {
-  // Assume the caller has the same max/min
   jsonStats["ArgumentRecord"]["_comment"] = "Histogram of function evaluations.";
+  jsonStats["ArgumentRecord"]["minArg"] = m_minArg;
+  jsonStats["ArgumentRecord"]["maxArg"] = m_maxArg;
   jsonStats["ArgumentRecord"]["histogramSize"] = m_histSize;
   for(unsigned int i=0; i<m_histSize; i++)
     jsonStats["ArgumentRecord"]["histogram"][std::to_string(i)]=mv_histogram[i];
-  jsonStats["ArgumentRecord"]["peak_arg"] = m_peak_arg;
+  jsonStats["ArgumentRecord"]["peak_index"] = m_peak_index;
   jsonStats["ArgumentRecord"]["m_min_recorded"] = m_min_recorded;
   jsonStats["ArgumentRecord"]["m_max_recorded"] = m_max_recorded;
   
@@ -171,8 +173,12 @@ inline void ArgumentRecord<IN_TYPE>::print_details(std::ostream& out)
 {
   out << "histogram: \n";
   out << this->to_string() << "\n";
-  out << "The peak_arg is located at x=" << m_peak_arg << " with ";
-  out << mv_histogram[COMPUTE_INDEX(m_peak_arg)] << " evaluations.\n";
+  out << "Args within the histogram's range were sampled"
+      << "the most often from the subinterval\n"
+  out << "["
+      << m_minArg + (m_maxArg - m_minArg)*m_peak_index/m_histSize << ", "
+      << m_minArg + (m_maxArg - m_minArg)*(m_peak_index+1)/m_histSize << ") "
+      << "with " << mv_histogram[m_peak_index] << " evaluations.\n";
   out << "The largest argument recorded was x=" << m_max_recorded << "\n";
-  out << "The most negative argument recorded was x=" << m_min_recorded << std::endl;
+  out << "The least argument recorded was x=" << m_min_recorded << std::endl;
 }
