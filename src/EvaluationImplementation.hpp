0d6767ad805cba2be6f805088ae72db7d03d5704
Shawn McAdam <ssm259@usask.ca>
2022-07-07 01:32:29 -0600

    added const qualifiers

--- src/EvaluationImplementation.hpp
+++ src/EvaluationImplementation.hpp
@@ -47,6 +47,7 @@ public:
     m_name(name), m_func(func), m_minArg(0), m_maxArg(0) {}
 
   virtual ~EvaluationImplementation(){};
+  EvaluationImplementation(EvaluationImplementation&&) = default;
 
   virtual OUT_TYPE operator()(IN_TYPE x) = 0;
   virtual void print_details(std::ostream& out)
@@ -56,10 +57,10 @@ public:
   virtual void print_details_json(std::ostream& out)=0;
 
   /* public access of protected data */
-  IN_TYPE min_arg(){ return m_minArg; };
-  IN_TYPE max_arg(){ return m_maxArg; };
-  unsigned order(){ return m_order; };
-  unsigned size(){ return m_dataSize; };
-  std::string name(){ return m_name; };
-  std::function<OUT_TYPE(IN_TYPE)> function(){ return m_func; };
+  IN_TYPE min_arg() const { return m_minArg; };
+  IN_TYPE max_arg() const { return m_maxArg; };
+  unsigned int order() const { return m_order; };
+  unsigned int size() const { return m_dataSize; };
+  std::string name() const { return m_name; };
+  std::function<OUT_TYPE(IN_TYPE)> function() const { return m_func; };
 };
--- src/TransferFunctionSinh.hpp
+++ src/TransferFunctionSinh.hpp
@@ -58,9 +58,10 @@ public:
   TransferFunctionSinh(IN_TYPE minArg, IN_TYPE tableMaxArg, IN_TYPE stepSize, std::array<IN_TYPE,4> inv_coefs) :
     m_minArg(minArg), m_tableMaxArg(tableMaxArg), m_stepSize(stepSize) { m_inv_coefs = inv_coefs; }
 
+  TransferFunctionSinh() = default;
   /* initialize the identity transfer function */
-  TransferFunctionSinh(IN_TYPE minArg, IN_TYPE tableMaxArg, IN_TYPE stepSize) :
-    TransferFunctionSinh<IN_TYPE>(minArg, tableMaxArg, stepSize, {-minArg/stepSize,1/stepSize,0,0}) {}
+  //TransferFunctionSinh(IN_TYPE minArg, IN_TYPE tableMaxArg, IN_TYPE stepSize) :
+  //  TransferFunctionSinh<IN_TYPE>(minArg, tableMaxArg, stepSize, {-minArg/stepSize,1/stepSize,0,0}) {}
 
   /* Build the coefficients in g_inv */
   template<typename OUT_TYPE>
@@ -72,12 +73,14 @@ public:
     // Template code is only compiled if the template is instantiated
     // so this will cause a compile time error only when this
     // constructor is called without Boost available:
-    static_assert(sizeof(IN_TYPE) != sizeof(IN_TYPE), "Cannot build a nonuniform grid without Boost verion 1.71.0 or higher");
+    static_assert(sizeof(IN_TYPE) != sizeof(IN_TYPE), "Cannot generate a nonuniform grid without Boost verion 1.71.0 or higher");
 #else
     using boost::math::quadrature::gauss_kronrod;
     using boost::math::differentiation::make_fvar;
 
-    // TODO check that the first derivative is not null
+    if(fc->autodiff1_func == nullptr)
+      throw std::invalid_argument("Error in func::TransferFunction. 1st derivative of function is needed to generate nonuniform grids but is null");
+
     // build a function to return the first derivative of f
     std::function<OUT_TYPE(IN_TYPE)> f_prime = [fc](IN_TYPE x) -> OUT_TYPE {
       return (fc->autodiff1_func)(make_fvar<IN_TYPE,1>(x)).derivative(1);
@@ -165,9 +168,7 @@ public:
       std::to_string(m_inv_coefs[1]) << "x + " << std::to_string(m_inv_coefs[0]) << std::endl;
   }
 
-  std::array<IN_TYPE,4> get_coefs() {
-    return m_inv_coefs;
-  }
+  std::array<IN_TYPE,4> get_coefs() const { return m_inv_coefs; }
 
   std::pair<IN_TYPE,IN_TYPE> arg_bounds_of_interval(){ return std::make_pair(m_minArg, m_tableMaxArg); }
 };
